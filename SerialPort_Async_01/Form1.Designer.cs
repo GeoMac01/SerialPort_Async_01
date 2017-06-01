6640589cac9d8fa4caa3dbd7826d4652e852063e
GeoMac01 <georges.maccario@excelitas.com>
2017-06-01 11:15:26 +0100

    Add project files.

---
+++ SerialPort_Async_01/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SerialPort_Async_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_Scan_Com = new System.Windows.Forms.Button();
+            this.bt_Connect_Com = new System.Windows.Forms.Button();
+            this.cb_COM_Sel = new System.Windows.Forms.ComboBox();
+            this.Lbl_Ls_Nb = new System.Windows.Forms.Label();
+            this.Bt_Send = new System.Windows.Forms.Button();
+            this.Rtb_UartIn = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Bt_TestRun = new System.Windows.Forms.Button();
+            this.Lbl_99Cmd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_Scan_Com
+            // 
+            this.bt_Scan_Com.Location = new System.Drawing.Point(121, 22);
+            this.bt_Scan_Com.Name = "bt_Scan_Com";
+            this.bt_Scan_Com.Size = new System.Drawing.Size(78, 28);
+            this.bt_Scan_Com.TabIndex = 0;
+            this.bt_Scan_Com.Text = "Scan COMs";
+            this.bt_Scan_Com.UseVisualStyleBackColor = true;
+            this.bt_Scan_Com.Click += new System.EventHandler(this.Bt_Scan_Com_Click);
+            // 
+            // bt_Connect_Com
+            // 
+            this.bt_Connect_Com.BackColor = System.Drawing.Color.Orange;
+            this.bt_Connect_Com.Location = new System.Drawing.Point(205, 22);
+            this.bt_Connect_Com.Name = "bt_Connect_Com";
+            this.bt_Connect_Com.Size = new System.Drawing.Size(78, 28);
+            this.bt_Connect_Com.TabIndex = 1;
+            this.bt_Connect_Com.Text = "Connect";
+            this.bt_Connect_Com.UseVisualStyleBackColor = false;
+            this.bt_Connect_Com.CausesValidationChanged += new System.EventHandler(this.Bt_Connect_Com_Click);
+            this.bt_Connect_Com.Click += new System.EventHandler(this.Bt_Connect_Com_Click);
+            // 
+            // cb_COM_Sel
+            // 
+            this.cb_COM_Sel.FormattingEnabled = true;
+            this.cb_COM_Sel.Location = new System.Drawing.Point(36, 22);
+            this.cb_COM_Sel.Name = "cb_COM_Sel";
+            this.cb_COM_Sel.Size = new System.Drawing.Size(68, 21);
+            this.cb_COM_Sel.TabIndex = 2;
+            // 
+            // Lbl_Ls_Nb
+            // 
+            this.Lbl_Ls_Nb.AutoSize = true;
+            this.Lbl_Ls_Nb.Location = new System.Drawing.Point(301, 30);
+            this.Lbl_Ls_Nb.Name = "Lbl_Ls_Nb";
+            this.Lbl_Ls_Nb.Size = new System.Drawing.Size(16, 13);
+            this.Lbl_Ls_Nb.TabIndex = 3;
+            this.Lbl_Ls_Nb.Text = "---";
+            // 
+            // Bt_Send
+            // 
+            this.Bt_Send.Location = new System.Drawing.Point(36, 94);
+            this.Bt_Send.Name = "Bt_Send";
+            this.Bt_Send.Size = new System.Drawing.Size(95, 24);
+            this.Bt_Send.TabIndex = 4;
+            this.Bt_Send.Text = "Send";
+            this.Bt_Send.UseVisualStyleBackColor = true;
+            this.Bt_Send.Click += new System.EventHandler(this.Bt_Send_Click);
+            // 
+            // Rtb_UartIn
+            // 
+            this.Rtb_UartIn.Location = new System.Drawing.Point(36, 124);
+            this.Rtb_UartIn.Name = "Rtb_UartIn";
+            this.Rtb_UartIn.Size = new System.Drawing.Size(95, 261);
+            this.Rtb_UartIn.TabIndex = 5;
+            this.Rtb_UartIn.Text = "";
+            this.Rtb_UartIn.DoubleClick += new System.EventHandler(this.Rtb_UartIn_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name ---";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ser. Numb. ---";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Part Nb. ---";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cust Part ---";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Date ---";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(202, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Firm ---";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(202, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Current ---";
+            // 
+            // Bt_TestRun
+            // 
+            this.Bt_TestRun.BackColor = System.Drawing.Color.Orange;
+            this.Bt_TestRun.Location = new System.Drawing.Point(36, 391);
+            this.Bt_TestRun.Name = "Bt_TestRun";
+            this.Bt_TestRun.Size = new System.Drawing.Size(95, 24);
+            this.Bt_TestRun.TabIndex = 13;
+            this.Bt_TestRun.Text = "Test Run";
+            this.Bt_TestRun.UseVisualStyleBackColor = false;
+            this.Bt_TestRun.Click += new System.EventHandler(this.Bt_TestRun_Click);
+            // 
+            // Lbl_99Cmd
+            // 
+            this.Lbl_99Cmd.AutoSize = true;
+            this.Lbl_99Cmd.Location = new System.Drawing.Point(334, 30);
+            this.Lbl_99Cmd.Name = "Lbl_99Cmd";
+            this.Lbl_99Cmd.Size = new System.Drawing.Size(16, 13);
+            this.Lbl_99Cmd.TabIndex = 14;
+            this.Lbl_99Cmd.Text = "---";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 452);
+            this.Controls.Add(this.Lbl_99Cmd);
+            this.Controls.Add(this.Bt_TestRun);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Rtb_UartIn);
+            this.Controls.Add(this.Bt_Send);
+            this.Controls.Add(this.Lbl_Ls_Nb);
+            this.Controls.Add(this.cb_COM_Sel);
+            this.Controls.Add(this.bt_Connect_Com);
+            this.Controls.Add(this.bt_Scan_Com);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_Scan_Com;
+        private System.Windows.Forms.Button bt_Connect_Com;
+        private System.Windows.Forms.ComboBox cb_COM_Sel;
+        private System.Windows.Forms.Label Lbl_Ls_Nb;
+        private System.Windows.Forms.Button Bt_Send;
+        private System.Windows.Forms.RichTextBox Rtb_UartIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Bt_TestRun;
+        private System.Windows.Forms.Label Lbl_99Cmd;
+    }
+}
+
---
+++ SerialPort_Async_01/Form1.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.IO;
+using System.Timers;
+
+namespace SerialPort_Async_01
+{
+    public partial class Form1 : Form
+    {
+
+        #region Commands Definition
+        // Command Constants
+        const string CmdLaserEnable = "02";
+        const string CmdSetLsPw = "03";
+        const string CmdRdSerialNo = "04";
+        const string CmdRdFirmware = "06";
+        const string CmdRdBplateTemp = "07";
+        const string CmdRdWavelen = "08";
+        const string CmdSetUnitNo = "12";
+        const string CmdLaserStatus = "14";
+        const string CmdRdTecTemprt = "15";
+        const string CmdHELP = "16";
+        const string CmdsetTTL = "17";
+        const string CmdEnablLogicvIn = "18";
+        const string CmdAnalgInpt = "19";
+        const string CmdRdLsrStatus = "20";
+        const string CmsSetPwMonOut = "21";
+        const string CmdRdCalDate = "22";
+        const string CmdSetPwCtrlOut = "23";
+        const string CmdSetOffstVolt = "24";
+        const string CmdSetVgaGain = "25";
+        const string CmdOperatingHr = "26";
+        const string CmdRdSummary = "27";
+        const string CmdSetInOutPwCtrl = "28";
+        const string CmdSet0mA = "29";
+        const string CmdSetStramind = "30";
+        const string CmdRdCmdStautus2 = "34";
+        const string CmdManufDate = "40";
+        const string CmdRdPwSetPcon = "41";
+        const string CmdRdInitCurrent = "42";
+        const string CmdRdModelName = "43";
+        const string CmdRdLaserPow = "44";
+        const string CmdRdPnNb = "45";
+        const string CmdRdCustomerPm = "46";
+        const string CmdRatedPower = "47";
+        const string CmdCurrentRead = "56";
+        const string CmdSetCalAPw = "60";
+        const string CmdSetCalBPw = "61";
+        const string CmdSetCalAPwtoVin = "62";
+        const string CmdSetCalBPwtoVin = "63";
+        const string CmdRstTime = "66";
+        const string CmdRstPtr = "67";
+        const string CmdRstTon = "68";
+        const string CmdRstCntr1000 = "69";
+        const string CmdSetFirmware = "70";
+        const string CmdSetSerNumber = "71";
+        const string CmdSetWavelenght = "72";
+        const string CmdSetLsMominalPw = "73";
+        const string CmdSetCustomerPm = "74";
+        const string CmdSetMaxIop = "76";
+        const string CmdSetCalDate = "77";
+        const string CmdSeManuDate = "78";
+        const string CmdSetPartNumber = "79";
+        const string CmdSetModel = "80";
+        const string CmdSetCalAPwtoI = "81";
+        const string CmdSetCalBPwtoI = "82";
+        const string CmdTestMode = "83";
+        const string CmdSetPSU = "84";
+        const string CmdRdPSUvolt = "85";//check cmd 86 / 85
+        const string CmdReadPSU = "86";
+        const string CmdSetBaseTempCal = "87";
+        const string CmdSetTECTemp = "90";
+        const string CmdSetTECkp = "91";
+        const string CmdSetTECki = "92";
+        const string CmdSetTECsmpTime = "93";
+        const string CmdRdTECsetTemp = "94";
+        const string CmdRdTECsetkp = "95";
+        const string CmdRdTECsetki = "96";
+        const string CmdRdTECsmpTime = "97";
+        const string CmdSetTECena_dis = "98";
+        const string CmdRdUnitNo = "99";
+        const string Footer = "\r\n";
+        const string Header = "#";
+        const string StrEnable = "0001";
+        const string StrDisable = "0000";
+        #endregion
+
+        string[] initial_Call = new string[7]
+        {
+            (CmdRdModelName),
+            (CmdRdSerialNo),
+            (CmdRdPnNb),
+            (CmdRdCustomerPm),
+            (CmdManufDate),
+            (CmdRdFirmware),
+            (CmdRdInitCurrent),
+        };
+
+        string[] portNames = new string[10];
+
+        string Outdata = string.Empty;
+        string Indata = string.Empty;
+        string laserAddress = string.Empty;
+        string cmdSubStr = string.Empty;
+        string rtnValue = string.Empty;
+        
+        int[] byteArrayToTest1 = new int[16];
+        int[] byteArrayToTest2 = new int[16];
+        int[] byteArrayToTest3 = new int[16];
+
+        bool dataReceived = false;
+        int pointerTestCycle = 0;
+        int arrLght = 0;
+        
+        SerialPort Serial_CDC = new SerialPort();
+        System.Timers.Timer Tmr_Send_Cmd = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitTimer();
+            
+            Tmr_Send_Cmd.Elapsed += new ElapsedEventHandler(Tmr_Send_Cmd_Elapsed); 
+            Serial_CDC.DataReceived += new SerialDataReceivedEventHandler(RS_DataReceivedHandler);
+        }
+        //================================================================================================
+        #region Timer_Init
+        private void InitTimer()
+        {
+            Tmr_Send_Cmd.Stop();
+            Tmr_Send_Cmd.Interval = 100;
+            Tmr_Send_Cmd.AutoReset = true;
+            Tmr_Send_Cmd.Enabled = false;
+        }
+        #endregion
+        //================================================================================================
+        #region GetPortName
+        private void GetPortNames()
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            
+            portNames = SerialPort.GetPortNames();
+
+            cb_COM_Sel.Items.Clear();
+            foreach (string s in portNames)
+            {
+                cb_COM_Sel.Items.Add(s);
+            }
+
+            if (cb_COM_Sel.Items.Count > 0)
+            {
+                cb_COM_Sel.SelectedIndex = 0;
+                Init_Coms(cb_COM_Sel.ToString());
+                bt_Connect_Com.Enabled = true;
+                bt_Connect_Com.BackColor = Color.Orange;
+            }
+            else
+            {
+                cb_COM_Sel.Text = "NO-COMs";
+                bt_Connect_Com.Enabled = false;
+                bt_Connect_Com.BackColor = Color.Red;
+            }
+
+            Task<bool> detect = DetectComs();//higher level
+
+            Cursor.Current = Cursors.Default;
+        }
+        #endregion
+        //================================================================================================
+        #region ASYNC Detect COMs Auto
+        private async Task<bool>  DetectComs()
+        {
+            bool goodComFlag = false;
+
+            foreach (string strCom in cb_COM_Sel.Items)
+            {
+                Serial_CDC.Close();
+                Serial_CDC.PortName = strCom;
+                Serial_CDC.Open();
+   
+                    RS_DataTransmit("00990000");
+                    bool firstGo = await WastingTime(500);//second level
+                    
+                    if ((Lbl_99Cmd.Text == CmdRdUnitNo) && goodComFlag == false)
+                    {
+                        goodComFlag = true;
+                        cb_COM_Sel.Text = strCom;
+                    }
+            }
+
+            if (goodComFlag == false) { MessageBox.Show("No Laser Connected"); }
+
+            return true;
+        }
+        #endregion
+        //================================================================================================
+        #region Init_Coms
+        private void Init_Coms(string comPort)
+        {
+            try
+            {
+                Serial_CDC.Close();
+                Serial_CDC.PortName = comPort;
+                Serial_CDC.Parity = Parity.None;
+                Serial_CDC.StopBits = StopBits.One;
+                Serial_CDC.DataBits = 8;
+                Serial_CDC.BaudRate = 115200;
+                //Serial_CDC.BaudRate = 19200;
+                Serial_CDC.ReadTimeout = 500;
+                Serial_CDC.WriteTimeout = 500;
+                Serial_CDC.ReceivedBytesThreshold = 10;
+            }
+            catch (Exception)
+            {
+                Serial_CDC.Close();
+                Serial_CDC.Dispose();
+                MessageBox.Show("USB_Port_Init COM Error");
+            }
+        }
+        #endregion
+        //================================================================================================
+        #region ScanCOM
+        private void Bt_Scan_Com_Click(object sender, EventArgs e)
+        {
+            GetPortNames();
+        }
+        #endregion
+        //================================================================================================
+        #region bt_Button_Com_Click
+        private void Bt_Connect_Com_Click(object sender, EventArgs e)
+        {
+            if (Serial_CDC.IsOpen)//COM oppened
+            {
+                Serial_CDC.Close();
+                bt_Connect_Com.BackColor = Color.Orange;
+                bt_Connect_Com.Text = "Connect";
+                bt_Scan_Com.Enabled = true;
+                cb_COM_Sel.Enabled = true;
+            }
+            else
+            {
+                try
+                {
+                    bt_Scan_Com.Enabled = false;
+                    cb_COM_Sel.Enabled = false;
+                    Serial_CDC.Close();//close port
+                    Serial_CDC.PortName = cb_COM_Sel.Text;
+                    Serial_CDC.Open();
+                    bt_Connect_Com.BackColor = Color.LawnGreen;
+                    bt_Connect_Com.Text = "Connected";
+                    Thread.Sleep(200);
+                }
+                catch
+                {
+                    Serial_CDC.Close();//close port
+                    Serial_CDC.Dispose();
+                    bt_Connect_Com.BackColor = Color.Orange;
+                    bt_Connect_Com.Text = "Connect";
+                    bt_Scan_Com.Enabled = true;
+                    cb_COM_Sel.Enabled = true;
+                    MessageBox.Show("COM Connect error");
+                }
+            }
+        }
+        #endregion
+        //================================================================================================
+        private void RS_DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            Indata = Serial_CDC.ReadExisting();
+            this.Invoke(new Action(() => DisplayText(Indata)));
+        }
+        //================================================================================================
+        private void Tmr_Send_Cmd_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            dataReceived = false;
+
+            if(pointerTestCycle > (arrLght - 1)) { pointerTestCycle = 0;}
+            RunTestArray(pointerTestCycle, StrDisable);
+            pointerTestCycle++;
+        }
+        //================================================================================================
+        private void RunTestArray(int posit, string argu)
+        {
+            string tempString = string.Empty;
+
+            tempString = (Lbl_Ls_Nb.Text + initial_Call[posit] + argu);
+            RS_DataTransmit(tempString);
+            //Task<bool> nextArr = WastingTime(50);
+        }
+        //================================================================================================
+        #region Display Select returned data
+        private void DisplayText(string rxData)
+        {
+                int strLength = 0;
+                dataReceived = true;
+
+                try
+                {
+                    strLength = rxData.Length;
+                    laserAddress = rxData.Substring(1, 2);
+                    cmdSubStr = rxData.Substring(3, 2);
+                    rtnValue = rxData.Substring(5, (strLength - 7));
+                    Debug.WriteLine("rtncmd:" + cmdSubStr + " " + "strLgth:" + strLength + " " + "rtnVal:" + rtnValue);
+
+                    switch (cmdSubStr)
+                    {
+                        case CmdRdUnitNo://will hapen if something valid is present
+                        Lbl_Ls_Nb.Text = laserAddress;
+                        Lbl_99Cmd.Text = cmdSubStr;
+                            break;
+
+                    case CmdRdModelName:
+                        label1.Text = rtnValue;
+                        break;
+
+                    case CmdRdSerialNo:
+                        label2.Text = rtnValue;
+                        break;
+
+                        case CmdRdPnNb:
+                        label3.Text = rtnValue;
+                        break;
+
+                    case CmdRdCustomerPm:
+                        label4.Text = rtnValue;
+                        break;
+
+                    case CmdManufDate:
+                        label5.Text = rtnValue;
+                        break;
+
+                    case CmdRdFirmware:
+                        label6.Text = rtnValue;
+                        break;
+
+                    case CmdRdInitCurrent:
+                        label7.Text = rtnValue;
+                        break;
+
+                        default:
+                            //MessageBox.Show("Invalid RX command");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No message");
+                    Trace.WriteLine("Error{0}", ex.ToString());
+                }
+        }
+        #endregion
+        //================================================================================================
+        private void Bt_Send_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            RS_DataTransmit("00990000");
+            Task<bool> rtn = WastingTime(1000);
+            pointerTestCycle = 0;
+            arrLght = initial_Call.Length;
+            Cursor.Current = Cursors.Default;
+        }
+        //================================================================================================
+        private void RS_DataTransmit(string thinksToTransmit)
+        {
+            try
+            {
+                Serial_CDC.Write(Header + thinksToTransmit + Footer);
+            }
+            catch (Exception)
+            {/*
+                Tmr_Send_Cmd.Stop();
+                Tmr_Send_Cmd.Dispose();
+                Serial_CDC.Close();
+                Serial_CDC.Dispose();
+                MessageBox.Show("Re-Scan for Laser", "COM error TX");
+                */
+            }
+            Debug.WriteLine("TX:" + thinksToTransmit);
+        }
+        //================================================================================================
+        private void Rtb_UartIn_DoubleClick(object sender, EventArgs e)
+        {
+            Tmr_Send_Cmd.Stop();
+            Rtb_UartIn.Clear();
+        }
+        //================================================================================================
+        private void Bt_TestRun_Click(object sender, EventArgs e)
+        {
+            if (Bt_TestRun.BackColor == Color.Orange)
+            {
+                Bt_TestRun.BackColor = Color.Green;
+                Tmr_Send_Cmd.Start();
+                //Task<bool> rtn = WastingTime(2000);
+            }
+            else if (Bt_TestRun.BackColor == Color.Green)
+            {
+                Bt_TestRun.BackColor = Color.Orange;
+                Tmr_Send_Cmd.Stop();
+            }
+        }
+        //================================================================================================
+        private async Task<bool> WastingTime(int timeWt)//not required if timer sloww for example
+        {
+            await Task.Delay(timeWt);//last level here
+            Debug.WriteLine("TaskEnd");
+            return true;
+        }
+        //================================================================================================
+
+        //===================================== END FORM ==================================================
+    }
+}
